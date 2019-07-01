ffc3e8a31949717e3ba67f1adef9db9c2adf93aa
rocco <r.righi@elsevier.com>
2019-07-01 20:12:07 +0100

    Add intermidiate part

--- index.js
+++ index.js
@@ -1,29 +1,53 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 
+
 function isInternalLink (link) {
-    console.log(link)
     const pattern = new RegExp('^\/.');
-    return link.match(pattern);
+    if(link) {
+        return link.match(pattern);
+    }
+    return false;
 }
 
-const options = {
-    uri: `https://www.monzo.com`,
-    transform: body => cheerio.load(body),
+function requestPage (path) {
+    const options = {
+        uri: `https://www.monzo.com${path}`,
+        transform: body => cheerio.load(body),
+    }
+    return rp(options);
 }
 
-function getBody (options) {
-    return rp(options)
-        .then($ => {
-            const internalLinks = $('a')
-                .toArray()
-                .map(link => $(link).attr('href'))
-                .filter(isInternalLink);
-            console.log(internalLinks);
-
-        })
-        .catch(function (err) {
-        });
+async function getInternalLinks (path) {
+    let $;
+    try {
+        $ = await requestPage(path);
+        const internalLinks = $('a')
+            .toArray()
+            .map(link => $(link).attr('href'))
+            .filter(isInternalLink);
+        return internalLinks;
+    } catch (err) {
+        console.log(err);
+    }
+}
+function createMap (map, key, limit) {
+    limit += 1;
+    if(limit > 4) return map;
+    const map = new Map();
+    map.set(key, createMap(), key, limit);
+    return map;
+}
+async function buildMap (siteMap=new Map(), path='/') {
+    if (siteMap.has(path)) {
+        const values = siteMap.get(path);
+        siteMap.set(path, values);
+        return;
+    }
+    const links = await getInternalLinks(path);
+    siteMap.set(path, links);
+    links.map(createMap);
+    console.log(siteMap)
 }
 
-getBody(options);
+buildMap();
