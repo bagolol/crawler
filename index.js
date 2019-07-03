66de9f41226b2e6921e39415ab5f86ffb646c728
rocco <r.righi@elsevier.com>
2019-07-03 11:24:57 +0100

    Add working solution with time

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 
+function calculateDuration(startTime, endTime) {
+    const timeDiff = endTime - startTime;
+    const seconds = Math.round( timeDiff / 1000);
+    console.log(seconds + " seconds");
+}
 
 function isInternalLink (link) {
     const pattern = new RegExp('^\/.');
-    const cdnPattern = new RegExp('^\/cdn-cgi');
+    const other = new RegExp('^\/(cdn-cgi|help|legal|blog|documents|static)')
     if(link) {
-        return link.match(pattern) && !link.match(cdnPattern);
+        return link.match(pattern) && !link.match(other);
     }
     return false;
 }
@@ -15,7 +20,7 @@ function requestPage (path) {
     const options = {
         uri: `https://www.monzo.com${path}`,
         transform: body => cheerio.load(body),
-    }
+    };
     return rp(options);
 }
 
@@ -23,22 +28,36 @@ async function getInternalLinks (path) {
     let $;
     try {
         $ = await requestPage(path);
-        const internalLinks = $('a')
-            .toArray()
-            .map(link => $(link).attr('href'))
-            .filter(isInternalLink);
-        return internalLinks;
+        return $('a')
+          .toArray()
+          .map(link => $(link).attr('href'))
+          .filter(isInternalLink);
     } catch (err) {
         console.log(err);
     }
 }
-async function buildMap (siteMap=new Map(), path='/') {
-    if (siteMap.has(path)) return;
+
+const visited = new Set();
+
+async function buildMap (siteMap= new Map(), path='/') {
     const links = await getInternalLinks(path);
+    visited.add(path);
     siteMap.set(path, links);
-    i += 1;
-    links.forEach(link => buildMap(siteMap, link));
-    console.log(siteMap);
+    const remainingLinks = links.filter(link => !siteMap.has(link));
+    for (const link of remainingLinks) {
+        if (!siteMap.has(link)) {
+            await buildMap(siteMap, link);
+        }
+    }
+    return siteMap;
 }
 
-buildMap();
+function main () {
+    const startTime = new Date();
+    return buildMap().then(res => {
+        console.log(res);
+        calculateDuration(startTime, new Date());
+    });
+}
+
+main();
